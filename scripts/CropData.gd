07642becbb11ee4f339166a3f1f320391fe92710
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2026-02-10 23:33:29 -0600

    Fix stale turns_changed self-connection in main scene, add crop data resources, reorganize project structure, add data resources, update scenes

---
+++ scripts/CropData.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name CropData
+
+@export var seed_pack: Texture2D
+@export var seed_post: Texture2D
+@export var crop_stage_1: Texture2D
+@export var crop_stage_2: Texture2D
+@export var crop_stage_3: Texture2D
+@export var crop_stage_4: Texture2D
+@export var final_crop: Texture2D
